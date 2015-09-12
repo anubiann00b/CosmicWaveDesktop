e8ab91a90393415f006664bdd39470e328a1edd2
Shreyas <skraman1999@gmail.com>
2015-09-12 05:50:06 -0400

    YO WE CAN GET THE FREQUENCYgit stgit stgit st

--- cosmicwave.py
+++ cosmicwave.py
@@ -1,3 +1,45 @@
-from CosmicWaveModem.cosmicmodem import encode, decode
+import CosmicWaveModem.cosmicmodem as cosmicmodem
+import alsaaudio, time, audioop
 
-print encode(bytearray([0x02]))
+from scipy.signal import fftconvolve
+from scipy import fftpack
+from pprint import pprint
+from numpy import argmax, sqrt, mean, diff, log
+from matplotlib.mlab import find
+
+def parabolic(f, x):
+    xv = 1/2. * (f[x-1] - f[x+1]) / (f[x-1] - 2 * f[x] + f[x+1]) + x
+    yv = f[x] - 1/4. * (f[x-1] - f[x+1]) * (xv - x)
+    return (xv, yv)
+
+def freq_from_autocorr(sig, fs):
+    # Calculate autocorrelation (same thing as convolution, but with 
+    # one input reversed in time), and throw away the negative lags
+    corr = fftconvolve(sig, sig[::-1], mode='full')
+    corr = corr[len(corr)/2:]
+    
+    # Find the first low point
+    d = diff(corr)
+    start = find(d > 0)[0]
+    
+    # Find the next peak after the low point (other than 0 lag).  This bit is 
+    # not reliable for long signals, due to the desired peak occurring between 
+    # samples, and other peaks appearing higher.
+    # Should use a weighting function to de-emphasize the peaks at longer lags.
+    peak = argmax(corr[start:]) + start
+    px, py = parabolic(corr, peak)
+    
+    return fs / px
+
+inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NORMAL)
+inp.setchannels(1)
+inp.setrate(4000)
+inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+
+inp.setperiodsize(160)
+
+while True:
+    # Read data from device
+    l, data = inp.read() # length is 160 bytes
+    dataArr = bytearray(data)
+    print freq_from_autocorr(dataArr, 4000)*2
