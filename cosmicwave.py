c6c4e989a4f65d1a299d2301e054753525e122a7
Shreyas <skraman1999@gmail.com>
2015-09-13 02:28:49 -0400

    WHOOO REDID DECODING AND IT WORKEDgit st!

--- cosmicwave.py
+++ cosmicwave.py
@@ -1,17 +1,20 @@
 import CosmicWaveModem.cosmicmodem as modem
 import alsaaudio
+from time import time
+from time import sleep
 
-inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NORMAL)
+inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NORMAL)
 inp.setchannels(1)
-inp.setrate(4000)
-inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-inp.setperiodsize(160)
+inp.setrate(modem.BITRATE) # 8000
+inp.setformat(alsaaudio.PCM_FORMAT_U8)
+inp.setperiodsize(modem.FRAMES) # 4000
 
 dataStream = []
 
 while True:
-    # Read data from device
+    # startTime = time()
     l, data = inp.read() # length is 160 bytes
+    # print 'time: ' + str(time() - startTime)
     decoded = modem.decode(bytearray(data))
     print decoded
     # dataStream.append(decoded)
